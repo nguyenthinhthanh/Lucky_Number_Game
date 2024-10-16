02cc1a65614d1c7d8f43072e2450fa633b5b7074
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2024-10-16 13:27:35 +0700

    Improve fsm for button

--- Lucky_number_game/Lucky_number_game.srcs/sources_1/new/fsmForButtonState.v
+++ Lucky_number_game/Lucky_number_game.srcs/sources_1/new/fsmForButtonState.v
@@ -24,13 +24,22 @@ module fsmForButtonState(
     input clk,
     input rst,
     input[3:0] button_in,                           /*This for read button value from Arty-z7*/
-    output reg[1:0] button_state[3:0],        /*This for button state*/
+    output reg[1:0] button_state0,                  /*This is state for button 0*/
+    output reg[1:0] button_state1,                  /*This is state for button 1*/
+    output reg[1:0] button_state2,                  /*This is state for button 2*/
+    output reg[1:0] button_state3,                  /*This is state for button 3*/
     output reg[3:0] button_read_done                /*This signal for feedback when read done in button*/
     );
     
     wire[3:0] button_press_flag_wire;
     wire[3:0] button_pressed_hold_flag_wire;
     
+    wire[1:0] button_state[3:0];
+    assign button_state[0] = button_state0;
+    assign button_state[1] = button_state1;
+    assign button_state[2] = button_state2;
+    assign button_state[3] = button_state3;
+     
     readButtonWithDebounce read_button_debounce_inst(
     .clk(clk),
     .rst(rst),
@@ -44,9 +53,11 @@ module fsmForButtonState(
     
     always @(posedge clk or posedge rst) begin
         if(rst) begin
-            for(i=0;i<`NUM_OF_BUTTON;i++) begin
-                button_state[i] <= `BUTTON_STATE_RELEASED;
-            end
+            button_state0 <= `BUTTON_STATE_RELEASED;
+            button_state1 <= `BUTTON_STATE_RELEASED;
+            button_state2 <= `BUTTON_STATE_RELEASED;
+            button_state3 <= `BUTTON_STATE_RELEASED;
+            
             button_read_done <= 4'b0000;
         end
         else begin
@@ -54,7 +65,19 @@ module fsmForButtonState(
                 case(button_state[i])
                     `BUTTON_STATE_RELEASED: begin
                         if(button_press_flag_wire[i]) begin
-                            button_state[i] <= `BUTTON_STATE_PRESSED;
+                            if(i==0) begin
+                                button_state0 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else if(i==1) begin
+                                button_state1 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else if(i==2) begin
+                                button_state2 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else begin
+                                /*i==3*/
+                                button_state3 <= `BUTTON_STATE_PRESSED;
+                            end
                             button_read_done[i] <= 1;
                         end
                         else begin
@@ -63,12 +86,36 @@ module fsmForButtonState(
                     end
                     `BUTTON_STATE_PRESSED: begin
                         if(!button_press_flag_wire[i]) begin
-                            button_state[i] <= `BUTTON_STATE_RELEASED;
+                            if(i==0) begin
+                                button_state0 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else if(i==1) begin
+                                button_state1 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else if(i==2) begin
+                                button_state2 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else begin
+                                /*i==3*/
+                                button_state3 <= `BUTTON_STATE_PRESSED;
+                            end
                             button_read_done[i] <= 1;
                         end
                         else begin
                             if(button_pressed_hold_flag_wire[i]) begin
-                                button_state[i] <= `BUTTON_STATE_PRESSED_HOLD;
+                                if(i==0) begin
+                                    button_state0 <= `BUTTON_STATE_PRESSED;
+                                end
+                                else if(i==1) begin
+                                    button_state1 <= `BUTTON_STATE_PRESSED;
+                                end
+                                else if(i==2) begin
+                                    button_state2 <= `BUTTON_STATE_PRESSED;
+                                end
+                                else begin
+                                    /*i==3*/
+                                    button_state3 <= `BUTTON_STATE_PRESSED;
+                                end
                                 button_read_done[i] <= 1;
                             end
                             else begin
@@ -78,7 +125,19 @@ module fsmForButtonState(
                     end
                     `BUTTON_STATE_PRESSED_HOLD: begin
                         if(!button_press_flag_wire[i]) begin
-                            button_state[i] <= `BUTTON_STATE_RELEASED;
+                            if(i==0) begin
+                                button_state0 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else if(i==1) begin
+                                button_state1 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else if(i==2) begin
+                                button_state2 <= `BUTTON_STATE_PRESSED;
+                            end
+                            else begin
+                                /*i==3*/
+                                button_state3 <= `BUTTON_STATE_PRESSED;
+                            end
                             button_read_done <= 1;
                         end
                         else begin
@@ -86,10 +145,22 @@ module fsmForButtonState(
                         end                
                     end
                     default: begin
-                        button_state[i] <= `BUTTON_STATE_RELEASED;
+                        if(i==0) begin
+                            button_state0 <= `BUTTON_STATE_PRESSED;
+                        end
+                        else if(i==1) begin
+                            button_state1 <= `BUTTON_STATE_PRESSED;
+                        end
+                        else if(i==2) begin
+                            button_state2 <= `BUTTON_STATE_PRESSED;
+                        end
+                        else begin
+                            /*i==3*/
+                            button_state3 <= `BUTTON_STATE_PRESSED;
+                        end
                         button_read_done[i] <= 0;
                     end
-                endcase
+                endcase      
             end
         end    
     end
