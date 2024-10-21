bd5c2d9ae6eaaee9fdd143427c914005dcec2a67
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2024-10-21 14:27:04 +0700

    Implement LuckyNumberGame /*Top module*/

---
+++ Lucky_number_game/Lucky_number_game.srcs/sources_1/new/LuckyNumberGame.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/21/2024 02:17:45 PM
+// Design Name: 
+// Module Name: LuckyNumberGame
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module LuckyNumberGame(
+
+    );
+endmodule
--- Lucky_number_game/Lucky_number_game.srcs/sources_1/new/fsmForButtonState.v
+++ Lucky_number_game/Lucky_number_game.srcs/sources_1/new/fsmForButtonState.v
@@ -26,19 +26,19 @@ module fsmForButtonState(
     input[3:0] button_in,                           /*This for read button value from Arty-z7*/
     output reg[7:0] button_state,                   /*This is for button state button_state[i*2 +:2] = 
                                                         button i*/
-    output reg[3:0] button_read_done                /*This signal for feedback when read done in button*/
+    //output reg[3:0] button_read_done                /*This signal for feedback when read done in button*/
     );
     
-    wire[3:0] button_press_flag_wire;
-    wire[3:0] button_pressed_hold_flag_wire;
+    wire[3:0] button_pressed_wire;
+    wire[3:0] button_pressed_hold_wire;
       
     readButtonWithDebounce read_button_debounce_inst(
     .clk(clk),
     .rst(rst),
     .button_in(button_in),
-    .button_press_flag(button_press_flag_wire),
-    .button_pressed_hold_flag(button_pressed_hold_flag_wire),
-    .button_read_done(button_read_done)
+    .button_pressed(button_pressed_wire),
+    .button_pressed_hold(button_pressed_hold_wire)
+    //.button_read_done(button_read_done)
     );
     
     integer i;
@@ -46,48 +46,48 @@ module fsmForButtonState(
     /*Check Fsm_for_button.drawio for FSM*/
     always @(posedge clk or posedge rst) begin
         if(rst) begin
-            button_read_done <= 4'b0000;
+            //button_read_done <= 4'b0000;
             button_state <= 8'b00000000;
         end
         else begin
             for(i=0;i<4;i=i+1) begin
                 case(button_state[i*2 +:2])
                     `BUTTON_STATE_RELEASED: begin
-                        if(button_press_flag_wire[i]) begin
+                        if(button_pressed_wire[i]) begin
                             button_state[i*2 +:2] <= `BUTTON_STATE_PRESSED;
-                            button_read_done[i] <= 1;
+                            //button_read_done[i] <= 1;
                         end
                         else begin
-                            button_read_done[i] <= 0;
+                            //button_read_done[i] <= 0;
                         end
                     end
                     `BUTTON_STATE_PRESSED: begin
-                        if(!button_press_flag_wire[i]) begin
+                        if(!button_pressed_wire[i]) begin
                             button_state[i*2 +:2] <= `BUTTON_STATE_RELEASED;
-                            button_read_done[i] <= 1;
+                            //button_read_done[i] <= 1;
                         end
                         else begin
-                            if(button_pressed_hold_flag_wire[i]) begin
+                            if(button_pressed_hold_wire[i]) begin
                                 button_state[i*2 +:2] <= `BUTTON_STATE_PRESSED_HOLD;
-                                button_read_done[i] <= 1;
+                                //button_read_done[i] <= 1;
                             end
                             else begin
-                                button_read_done[i] <= 0;
+                                //button_read_done[i] <= 0;
                             end
                         end
                     end
                     `BUTTON_STATE_PRESSED_HOLD: begin
-                        if(!button_press_flag_wire[i]) begin
+                        if(!button_pressed_wire[i]) begin
                             button_state[i*2 +:2] <= `BUTTON_STATE_RELEASED;
-                            button_read_done <= 1;
+                            //button_read_done <= 1;
                         end
                         else begin
-                            button_read_done <= 0;
+                            //button_read_done <= 0;
                         end                
                     end
                     default: begin
                         button_state[i*2 +:2] <= `BUTTON_STATE_RELEASED;
-                        button_read_done[i] <= 0;
+                        //button_read_done[i] <= 0;
                     end
                 endcase      
             end
