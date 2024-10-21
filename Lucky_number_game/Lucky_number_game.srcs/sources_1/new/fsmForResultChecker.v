22f19e65a1f1efb4cc21028cba7b7afd07a1bff3
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2024-10-21 10:38:55 +0700

    Implement for fsmResultChecker

---
+++ Lucky_number_game/Lucky_number_game.srcs/sources_1/new/fsmForResultChecker.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/21/2024 10:38:17 AM
+// Design Name: 
+// Module Name: fsmForResultChecker
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fsmForResultChecker(
+
+    );
+endmodule
